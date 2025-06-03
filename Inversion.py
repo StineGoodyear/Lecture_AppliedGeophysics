c91e3d9eb50a005d4ebc5cab7ae9c2621a1169c5
Stine <56740918+StineGoodyear@users.noreply.github.com>
2025-06-03 09:14:51 +0200

    Add files via upload

--- Inversion.py
+++ Inversion.py
@@ -14,7 +14,7 @@ def precompute_bessel_function(L_model,dx,n_max):
 
 def forward_model_1(rho,d,L_model,bessel,dx,n_max): 
     ''' Berechnet die scheinbaren spezifischen Widerstände für Schichten mit der
-        Dicke d und spezifischen Widerständen rho für bestimmte Auslagenlängen L/2 
+        Mächtigkeit d und spezifischen Widerständen rho für bestimmte Auslagenlängen L/2 
 
     © Nepomuk Boitz, Mai 2020, boitz@geophysik.fu-berlin.de
 '''
@@ -56,6 +56,7 @@ def forward_model_1(rho,d,L_model,bessel,dx,n_max):
     return rho_a
 
 def invert_model(n_realisation,it_max,start_model,data,dx,n_max,fac):
+   # print(n_realisation)    
     history = np.zeros([n_realisation,len(start_model)+1])
     bessel = precompute_bessel_function(data[:,0],dx,n_max)
     n_layer = len(start_model)//2+1
@@ -88,14 +89,16 @@ def invert_model(n_realisation,it_max,start_model,data,dx,n_max,fac):
 
 def plot_single_model(data,rho_a): 
     
-    plt.loglog(data[:,0],data[:,2],'b.-')
-    plt.loglog(data[:,0],rho_a,'r.-')
+    #plt.loglog(data[:,0],data[:,2],'b.-')
+    plt.semilogy(data[:,0],data[:,2],'b.-')
+#    plt.loglog(data[:,0],rho_a,'r.-')
+    plt.semilogy(data[:,0],rho_a,'r.-')
     plt.xlabel('L/2 [m]')
     plt.ylabel('scheinbarer spezifischer Widerstand [Ohm m]')
     plt.legend(['Data','Modell-Fit'])
     plt.title('Datenanpassung durch das beste invertierte Modell')
     
-def plot_equivalent_models(history):
+def plot_equivalent_models(history,d_max,rho_max):
     #fig, ax = plt.subplots(figsize=(8, 8))
 
     n_layer = np.size(history,1)//2
@@ -107,18 +110,20 @@ def plot_equivalent_models(history):
         for j in range(n_layer-1):
             z[range(j*2+1,j*2+3)] = history[i,n_layer+j] + z[j]
         z[n_layer*2-1] = 20
-        plt.loglog(rho,z,'k--')
+        #plt.loglog(rho,z,'k--')
+        plt.plot(rho,z,'k--')
         if np.argmin(history[:,np.size(history,1)-1])==i:
-            plt.loglog(rho,z,'r-')
-    plt.ylim((0.1,50))  
-    plt.xlim((0.1,500))   
+#            plt.loglog(rho,z,'r-')
+            plt.plot(rho,z,'r-')	
+    plt.ylim((0.1,d_max))  
+    plt.xlim((0.1,rho_max))   
     plt.gca().invert_yaxis()
     plt.ylabel('Tiefe [m]')
     plt.xlabel('Spezifischer Widerstand [Ohm m]')
     plt.title('Äquivalenzmodelle')
     
     
-def plot_final_model(history):
+def plot_final_model(history,d_max,rho_max):
 
     n_layer = np.size(history,1)//2
     for i in range(np.size(history,0)):
@@ -131,15 +136,16 @@ def plot_final_model(history):
         z[n_layer*2-1] = 20
         #plt.loglog(rho,z,'k--')
         if np.argmin(history[:,np.size(history,1)-1])==i:
-            plt.loglog(rho,z,'r-')
-    plt.ylim((0.1,50))    
-    plt.xlim((0.1,500))
+            #plt.loglog(rho,z,'r-')
+            plt.plot(rho,z,'r-')
+    plt.ylim((0.1,d_max))    
+    plt.xlim((0.1,rho_max))
     plt.gca().invert_yaxis()
     plt.ylabel('Tiefe [m]')
     plt.xlabel('Spezifischer Widerstand [Ohm m]')
     plt.title('Das beste invertierte Modell')
     
-def plot_start_model(rho_start,d_start,d_max):
+def plot_start_model(rho_start,d_start,d_max,rho_max):
     
     model = np.zeros([len(rho_start)*2,2])
     ds = []
@@ -153,9 +159,10 @@ def plot_start_model(rho_start,d_start,d_max):
         model[i+1,1] = rho_start[numi]
         model[i+1:i+3,0] = ds[numi]
         numi += 1
-    plt.ylim((0.1,50))   
-    plt.xlim((0.1,500))
-    plt.loglog(model[:,1],model[:,0],'-r')
+    plt.ylim((0.1,d_max))   
+    plt.xlim((0.1,rho_max))
+    #plt.loglog(model[:,1],model[:,0],'-r')
+    plt.plot(model[:,1],model[:,0],'-r')
     plt.gca().invert_yaxis()
     plt.ylabel('Tiefe [m]')
     plt.xlabel('spezifischer Widerstand [Ohm m]')
@@ -168,4 +175,74 @@ def plot_sondierungskurve(L_halbe,rho_a):
     plt.ylabel('scheinbarer spezifischer Widerstand [Ohm m]')
     plt.title('Sondierungskurve')
     
+ 
+def get_inversion_result(a,L_halbe, rho_a, rho_start, d_start):
+    d_max = 30
+    rho_max = 600
+
+    # Messwerte
+    # Halber Abstand der Einspeisesonden i 
+    L_halbe = np.array(L_halbe)
+    # Scheinbarer spezifischer Widerstand in Ohm Meter
+    rho_a = np.array(rho_a)
+    
+    #-----------------------------------
+    # Startmodell original
+    rho_start = np.array(rho_start); 
+    d_start = np.array(d_start); 
+    #-----------------------------------
+    
+    G=np.multiply(math.pi,(np.square(L_halbe)-np.square(a/2)))
+    
+    # Inversionparameter
+    dx = .025 # Parameter für das numerische Lösen des Intergrals (am besten so lassen)
+    n_max = 10; # Parameter für das numerische Lösen des Intergrals (am besten so lassen)
+    fac = .15; # kontrolliert wie groß ein einzelnes Modellupdate sein soll
+    it_max = 500; # Anzahl der Modelle pro Iteration, die getestet werden
+    n_realisation = 10 ; # Wie viele Equivalenzmodelle sollen berechnet werden ? 
         
+    # Inversion wird gestartet 
+    data = np.column_stack((L_halbe,G,rho_a))
+    start_model = np.concatenate([rho_start,d_start])
+
+    bessel = precompute_bessel_function(data[:,0],dx,n_max)
+    
+    plt.figure(figsize=(20,40))
+    #fig, ax = plt.subplots(2, 2)
+    #fig.tight_layout(h_pad=2)
+    #plt.subplot(4,2,1)
+    #plot_sondierungskurve(L_halbe,rho_a)
+    
+    plt.subplot(4,2,2)
+    history = invert_model(n_realisation,it_max,start_model,data,dx,n_max,fac)
+    
+    plt.subplot(4,2,1)
+    plot_start_model(rho_start,d_start,d_max,rho_max)
+    
+    # Find best model 
+    plt.subplot(4,2,4)
+    which_model = np.argmin(history[:,np.size(history,1)-1])
+    
+    # Plot best model
+    
+    rho_final = history[which_model,range(0,np.size(history,1)//2)]; 
+    d_final = history[which_model,range(np.size(history,1)//2,np.size(history,1)-1)]; 
+    rho_aM = forward_model_1(rho_final,d_final,data[:,0],bessel,dx,n_max)
+    plot_single_model(data,rho_aM)
+    
+    plt.subplot(4,2,3)
+    plot_final_model(history,d_max,rho_max)
+    
+    # Plot Equivalent-Models
+    plt.subplot(4,2,5)
+    plot_equivalent_models(history,d_max,rho_max)
+    
+    print('  ')
+    print(' ************** ')
+    print('Best Fit Model:')
+    print('spez. Widerstand: ',rho_final)
+    print('Schichtmächtigkeit: ', d_final)    
+           
+    print('Fertig')
+         
+    	  	       
